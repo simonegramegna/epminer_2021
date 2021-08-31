63092504542bf9b20b3f551ac09fa0b94d4e2791
simone_pc <gramegna.simone@gmail.com>
2021-08-31 21:08:11 +0200

    test con javafx e material design -1

---
+++ epminerGui/src/main/java/map/MainWin.java
@@ -0,0 +1,18 @@
+package map;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainWin extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+/*
+        Scene scene = new Scene(new StackPane(new Label("map")), width: 400, height: 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();*/
+    }
+}
