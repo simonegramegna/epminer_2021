4015938b4c0a361adff627b89381f3573f121915
simone_pc <gramegna.simone@gmail.com>
2021-09-17 19:46:40 +0200

    modificato input per tabella target e background

--- estensione/src/main/java/WinInput.java
+++ estensione/src/main/java/WinInput.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -27,8 +21,6 @@ public class WinInput extends javax.swing.JPanel {
         panelOut = out;
     }
 
-    private static String availableTables[] = { "playtennisbackground", "playtennistarget" };
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -46,11 +38,10 @@ public class WinInput extends javax.swing.JPanel {
         inputGrowRate = new javax.swing.JTextField();
         jSeparator1 = new javax.swing.JSeparator();
         targetLabel = new javax.swing.JLabel();
-        targetInput = new javax.swing.JTextField();
         backgroundLabel = new javax.swing.JLabel();
-        backgroundInput = new javax.swing.JTextField();
-        detailBtn = new javax.swing.JButton();
         btnInput = new javax.swing.JButton();
+        targetChoose = new javax.swing.JComboBox<>();
+        backgroundChoose = new javax.swing.JComboBox<>();
 
         setPreferredSize(new java.awt.Dimension(600, 500));
 
@@ -60,31 +51,14 @@ public class WinInput extends javax.swing.JPanel {
         grLabel.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         grLabel.setText("Valore minimo di Grow Rate");
 
-        inputGrowRate.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                inputGrowRateActionPerformed(evt);
-            }
-        });
-
         targetLabel.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         targetLabel.setText("Tabella Target");
-
-        targetInput.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                targetInputActionPerformed(evt);
-            }
-        });
 
         backgroundLabel.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         backgroundLabel.setText("Tabella Background");
 
-        detailBtn.setText("Visualizza tabelle presenti");
-        detailBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                detailBtnActionPerformed(evt);
-            }
-        });
-
+        btnInput.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        btnInput.setForeground(new java.awt.Color(30, 0, 255));
         btnInput.setText("VAI!");
         btnInput.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -92,17 +66,17 @@ public class WinInput extends javax.swing.JPanel {
             }
         });
 
+        targetChoose.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        targetChoose.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "playtennistarget" }));
+
+        backgroundChoose.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        backgroundChoose.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "playtennisbackground" }));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(33, 33, 33)
-                .addComponent(detailBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnInput, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(65, 65, 65))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
@@ -111,11 +85,11 @@ public class WinInput extends javax.swing.JPanel {
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                                 .addComponent(backgroundLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(backgroundInput, javax.swing.GroupLayout.PREFERRED_SIZE, 380, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(backgroundChoose, javax.swing.GroupLayout.PREFERRED_SIZE, 333, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                                 .addComponent(targetLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(targetInput))))
+                                .addComponent(targetChoose, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
@@ -127,8 +101,11 @@ public class WinInput extends javax.swing.JPanel {
                         .addGap(35, 35, 35)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(inputGrowRate, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(inputMinSup, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(32, Short.MAX_VALUE))
+                            .addComponent(inputMinSup, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(210, 210, 210)
+                        .addComponent(btnInput, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(79, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -144,45 +121,33 @@ public class WinInput extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(targetLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(targetInput, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(8, 8, 8)
+                        .addComponent(targetChoose)))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(backgroundLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(backgroundInput, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(44, 44, 44)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(detailBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnInput, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                    .addComponent(backgroundChoose, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(39, 39, 39)
+                .addComponent(btnInput, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(160, 160, 160))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void targetInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_targetInputActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_targetInputActionPerformed
-
-    private String getInfoTableMessage() {
-        return "Le tabelle presenti sono: \n"
-                + java.util.Arrays.toString(availableTables).replace("]", "").replace("[", "");
-    }
-
-    private void inputGrowRateActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_inputGrowRateActionPerformed
-        // TODO add your handling code here:
-    }// GEN-LAST:event_inputGrowRateActionPerformed
-
+    
     private void btnInputActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_btnInputActionPerformed
 
         String minSup = inputMinSup.getText();
         String minGrow = inputGrowRate.getText();
-        String targetTable = targetInput.getText();
-        String backgroundTable = backgroundInput.getText();
+        //String targetTable = targetInput.getText();
+        //String backgroundTable = backgroundInput.getText();
 
         float sentMinSup = -1;
         float sentMinGrow = -1;
-        String sentTargetTable = "";
-        String sentBackgroundTable = "";
+        String sentTargetTable = (String) backgroundChoose.getSelectedItem();
+        String sentBackgroundTable = (String) targetChoose.getSelectedItem();;
 
         if (minSup.equals("")) {
             JOptionPane.showMessageDialog(null, "Inserire un valore di minimo supporto", "Errore",
@@ -215,43 +180,9 @@ public class WinInput extends javax.swing.JPanel {
                 }
             }
         }
-
-        if (targetTable.equals("")) {
-            JOptionPane.showMessageDialog(null, "Inserire per la tabella target", "Errore", JOptionPane.ERROR_MESSAGE);
-
-            targetInput.setText("");
-        } else {
-
-            if (Utils.checkStringArray(availableTables, targetTable)) {
-
-                sentTargetTable = targetTable;
-            } else {
-                JOptionPane.showMessageDialog(null, "La tabella inserita non e' presente tra le tabelle", "Errore",
-                        JOptionPane.ERROR_MESSAGE);
-                targetInput.setText("");
-            }
-        }
-
-        if (backgroundTable.equals("")) {
-            JOptionPane.showMessageDialog(null, "Inserire per la tabella target", "Errore", JOptionPane.ERROR_MESSAGE);
-
-            backgroundInput.setText("");
-        } else {
-
-            if (Utils.checkStringArray(availableTables, backgroundTable)) {
-
-                sentBackgroundTable = backgroundTable;
-            } else {
-                JOptionPane.showMessageDialog(null, "La tabella inserita non e' presente tra le tabelle", "Errore",
-                        JOptionPane.ERROR_MESSAGE);
-                backgroundInput.setText("");
-            }
-        }
-
-        if ((sentMinSup > 0 && sentMinSup <= 1) && (sentMinGrow > 0)
-                && Utils.checkStringArray(availableTables, sentBackgroundTable)
-                && Utils.checkStringArray(availableTables, sentTargetTable)) {
-
+       
+        if ((sentMinSup > 0 && sentMinSup <= 1) && sentMinGrow > 0) {
+            
             try{
                 panelOut.writeObject(sentMinSup);
                 panelOut.writeObject(sentMinGrow);
@@ -266,14 +197,17 @@ public class WinInput extends javax.swing.JPanel {
                 MainWinClient topFrame = (MainWinClient) SwingUtilities.getWindowAncestor(this);
                 topFrame.resetMainPanel();
                 
-             WinOutEp dialog = new WinOutEp(new javax.swing.JFrame(), true, epMiner, fpMiner, sentMinSup, sentMinGrow, sentTargetTable, sentBackgroundTable, fpMiner, epMiner, panelOut);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+            WinOutEp dialog = new WinOutEp(new javax.swing.JFrame(), true, epMiner, fpMiner, sentMinSup, sentMinGrow, sentTargetTable, sentBackgroundTable, fpMiner, epMiner, panelOut);
+               
+            dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
                         System.exit(0);
                     }
                 });
                 dialog.setVisible(true);
+                
             } catch (IOException e) {
                 JOptionPane.showMessageDialog(null, "Errore invio dei dati ", "Qualcosa e' andato storto ",
                 JOptionPane.ERROR_MESSAGE);
@@ -281,39 +215,27 @@ public class WinInput extends javax.swing.JPanel {
                 JOptionPane.showMessageDialog(null, "Errore invio dei dati ", "Qualcosa e' andato storto ",
                 JOptionPane.ERROR_MESSAGE);
             }
-        
-          
         } else {
             JOptionPane.showMessageDialog(null, "I valori inseriti non sono validi ", "Qualcosa e' andato storto ",
                     JOptionPane.ERROR_MESSAGE);
         }
-
     }// GEN-LAST:event_btnInputActionPerformed
 
-    private void detailBtnActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_detailBtnActionPerformed
-
-        JOptionPane.showMessageDialog(this, getInfoTableMessage());
-    }// GEN-LAST:event_detailBtnActionPerformed
-    
     private void resetInput(){
-        
-        backgroundInput.setText("");
         inputGrowRate.setText("");
         inputMinSup.setText("");
-        targetInput.setText("");
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField backgroundInput;
+    private javax.swing.JComboBox<String> backgroundChoose;
     private javax.swing.JLabel backgroundLabel;
     private javax.swing.JButton btnInput;
-    private javax.swing.JButton detailBtn;
     private javax.swing.JLabel grLabel;
     private javax.swing.JTextField inputGrowRate;
     private javax.swing.JTextField inputMinSup;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JLabel supLabel;
-    private javax.swing.JTextField targetInput;
+    private javax.swing.JComboBox<String> targetChoose;
     private javax.swing.JLabel targetLabel;
     // End of variables declaration//GEN-END:variables
 }
