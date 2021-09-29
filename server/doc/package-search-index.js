c234009750e88e9bae7a4ca65bde9dc0d090a717
simone_zorinOS <gramegna.simone@gmail.com>
2021-09-29 10:14:32 +0200

    aggiunto javadoc

--- server/doc/package-search-index.js
+++ server/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"data"},{"l":"database"},{"l":"keyboardinput"},{"l":"mining"},{"l":"server"},{"l":"utility"}];updateSearchResults();
